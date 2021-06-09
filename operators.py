dc4e53bb60bceef67249252d7ccfe93c988df07d
aayush pradhan <78127930+aayush1123@users.noreply.github.com>
2021-06-09 08:39:44 +0545

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,14 @@
+a=4
+b=2
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)# it give float value
+print("this is remainder",5%2)#4/2=2 so there is no remainder. it give remainder as result
+print(a**b)#this is a to the power b
+a=32
+b=5
+print(a//b)#if a=9 and b=2 then 9/2=4.5 but it give the result without the decimal value
+x,y=2,2
+x+=y#instead of X=x+y we can write x+=y
+print(x)
